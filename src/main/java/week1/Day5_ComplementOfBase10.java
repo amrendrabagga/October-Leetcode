e4b7bdfd6642a77ddb6fd406168f86b0a69a0fb1
Amrendra Bagga <amrendrabagga@gmail.com>
2020-10-05 23:47:05 +0530

    week1 day5 ComplementOfBase10 number commit

---
+++ src/main/java/week1/Day5_ComplementOfBase10.java
@@ -0,0 +1,21 @@
+package week1;
+
+public class Day5_ComplementOfBase10 {
+    public int bitwiseComplement(int N) {
+        int len = Integer.toBinaryString(N).length();
+        int res = 0;
+        for (int i=0; i<len; i++) {
+            int b = (1 << i);
+            if ((N & b) != 0)
+                res |= 0 << 1;
+            else
+                res |= 1 << i;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Day5_ComplementOfBase10 obj = new Day5_ComplementOfBase10();
+        System.out.println(obj.bitwiseComplement(5));
+    }
+}
