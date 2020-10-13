738b4a2ec78c8e23d1265fc70e3b1fa8f95f59f1
Amrendra Bagga <amrendrabagga@gmail.com>
2020-10-14 00:48:07 +0530

    week2 day6 SortLinkList commit

---
+++ src/main/java/week2/Day6_SortLinkList.java
@@ -0,0 +1,44 @@
+package week2;
+
+import utils.ListNode;
+
+public class Day6_SortLinkList {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = middleNode(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null;
+        return mid;
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode current = dummyNode;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        current.next = l1 != null ? l1 : l2;
+        return dummyNode.next;
+    }
+}
