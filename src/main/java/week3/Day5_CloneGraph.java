215a71c06022769ad9a1468c78c78080f7494814
Amrendra Bagga <amrendrabagga@gmail.com>
2020-10-20 22:09:23 +0530

    week3 day5 deep clone connected graph

---
+++ src/main/java/week3/Day5_CloneGraph.java
@@ -0,0 +1,30 @@
+package week3;
+
+import utils.Node;
+
+import java.util.*;
+
+public class Day5_CloneGraph {
+    public Node cloneGraph(Node node) {
+        if (node == null)
+            return node;
+        // going with bfs, easier than dfs:)
+        Map<Node, Node> map = new HashMap();
+        map.put(node, new Node(node.val, new ArrayList<>()));
+
+        Queue<Node> queue = new LinkedList();
+        queue.offer(node);
+
+        while (!queue.isEmpty()) {
+            Node n = queue.poll();
+            for (Node t : n.neighbors) {
+                if (!map.containsKey(t)) {
+                    map.put(t, new Node(t.val, new ArrayList<>()));
+                    queue.offer(t);
+                }
+                map.get(n).neighbors.add(map.get(t));
+            }
+        }
+        return map.get(node);
+    }
+}
